2bd8a89ee24b9aa5c3198f546641c4c422a8f2c8
Abhi <you@example.com>
2017-09-24 11:38:47 -0700

    add start.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import request, render_template, Flask
 import  os, sys, json
 import requests
+import random
 app = Flask(__name__)
 
 from random import randint
@@ -26,6 +27,24 @@ def send():
     voice = VoiceClient(customer_id, api_key)
     response1 = voice.call(phone_number, message, message_type)
     return "success"
+
+
+@app.route('/forward')
+def forward():
+    args = request.args
+    customer_id = "2C1097F6-3917-4A53-9D38-C45A3C8ADD2B"
+    api_key = "FTgHUVjcPWvgzCvtksi2v+tMLTAXbh5LLVEl1Wcl4NAtszxElZL4HS/ZwJqJufRkEmRpwUTwULxsZgL2c649vQ=="
+    phone_number = "14084299128"
+    messages = ["Hello, citibank is offering you an amazing discount on your favorite armani watch!", "Invest in this abc policy today and after 3 months you can try your favorite sky diving!"]
+    message = random.choice(messages)
+    message_type = "ARN"
+    messaging = MessagingClient(customer_id, api_key)
+    response2 = messaging.message(phone_number, message, message_type)
+    voice = VoiceClient(customer_id, api_key)
+    response1 = voice.call(phone_number, message, message_type)
+    return render_template('forward.html')
+
+
     
 if __name__ == '__main__':
 	app.secret_key = os.urandom(12)
