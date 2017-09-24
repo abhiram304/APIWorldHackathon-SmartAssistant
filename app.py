594d140b6bb344699495d57ff96640aef60de572
Abhi <you@example.com>
2017-09-24 12:26:12 -0700

    add start.html

--- app.py
+++ app.py
@@ -35,13 +35,15 @@ def forward():
     customer_id = "2C1097F6-3917-4A53-9D38-C45A3C8ADD2B"
     api_key = "FTgHUVjcPWvgzCvtksi2v+tMLTAXbh5LLVEl1Wcl4NAtszxElZL4HS/ZwJqJufRkEmRpwUTwULxsZgL2c649vQ=="
     phone_number = "14084299128"
-    messages = ["Hello, citibank is offering you an amazing discount on your favorite armani watch!", "Invest in this abc policy today and after 3 months you can try your favorite sky diving!"]
-    message = random.choice(messages)
+    messages2 = ["Hello, citibank is offering you an amazing discount on your favorite armani watch! ", "Invest in this abc policy today and after 3 months you can try your favorite sky diving!"]
+    messages1 = ["Hello, citibank is offering you an amazing discount on your favorite armani watch!  Link : https://goo.gl/DYVALW", "Invest in this abc policy today and after 3 months you can try your favorite sky diving!"]
+    message1 = random.choice(messages1)
+    message2 = random.choice(messages2) 
     message_type = "ARN"
     messaging = MessagingClient(customer_id, api_key)
-    response2 = messaging.message(phone_number, message, message_type)
+    response2 = messaging.message(phone_number, message1, message_type)
     voice = VoiceClient(customer_id, api_key)
-    response1 = voice.call(phone_number, message, message_type)
+    response1 = voice.call(phone_number, message2, message_type)
     return render_template('forward.html')
 
 
